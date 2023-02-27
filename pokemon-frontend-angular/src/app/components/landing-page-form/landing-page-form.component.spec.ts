1be7c6e0c311163d009ed458dbc268a0bcc30d5c
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-02-27 14:45:09 +0100

    added components for the view ti display
    This commit isin charge of dividing the view component to smaller pieces

---
+++ pokemon-frontend-angular/src/app/components/landing-page-form/landing-page-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LandingPageFormComponent } from './landing-page-form.component';
+
+describe('LandingPageFormComponent', () => {
+  let component: LandingPageFormComponent;
+  let fixture: ComponentFixture<LandingPageFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LandingPageFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LandingPageFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ pokemon-frontend-angular/src/app/components/landing-page-form/landing-page-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-form',
+  templateUrl: './landing-page-form.component.html',
+  styleUrls: ['./landing-page-form.component.css']
+})
+export class LandingPageFormComponent {
+
+}
