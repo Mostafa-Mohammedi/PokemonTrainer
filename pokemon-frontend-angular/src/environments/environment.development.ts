da785dff7f433fb9e2823015773de96453647445
mariehavre <havre.marie@gmail.com>
2023-02-28 11:32:39 +0100

    add environment variables

--- pokemon-frontend-angular/src/environments/environment.development.ts
+++ pokemon-frontend-angular/src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    apiTrainer: 'https://ripple-cyber-angolatitan.glitch.me/trainers',
+    apiKey: 'c6gXZwhbmoAnaW3NiHQqGP641l0mlR5C0ZDyuXCnJhNyP8yjPBxXg19OKSQOsLra',
+    apiPokemon: 'https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0',
+    apiPokemonSpecific: 'https://pokeapi.co/api/v2/pokemon',
+    pokemonImages: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
+};
