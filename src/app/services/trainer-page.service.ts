607dd0c4189b44d03029338ef9402fa0c776cc38
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-03-03 16:48:14 +0100

    added comment for method in different component and services

--- src/app/services/trainer-page.service.ts
+++ src/app/services/trainer-page.service.ts
@@ -13,6 +13,13 @@ const { apiKey, apiTrainer } = environment
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+
+Boolean indicating if the service is currently loading data
+@private
+@type {boolean}
+*/
 export class TrainerPageService {
   private _loading: boolean = false;
 
@@ -22,9 +29,23 @@ export class TrainerPageService {
     private readonly trainerService: TrainerServiceService
   ) { }
 
+  /**
+Getter method for the loading property
+@readonly
+@type {boolean}
+@memberof TrainerPageService
+*/
   get loading(): boolean {
     return this._loading;
   }
+
+  /**
+Method for adding a pokemon to the trainer's array
+@public
+@param {string} pokemonName - The name of the pokemon to add
+@throws Will throw an error if the trainer or pokemon doesn't exist
+@returns {Observable<Trainer>} An observable containing the updated trainer data
+*/
 
   public addToPokemonTrainer(pokemonName: string): Observable<Trainer> {
 
@@ -33,12 +54,12 @@ export class TrainerPageService {
     }
     const trainer: Trainer = this.trainerService.trainer;
 
-    const pokemon: Pokemon |undefined = this.pokemonCatalogueService.pokemonByName(pokemonName);
+    const pokemon: Pokemon | undefined = this.pokemonCatalogueService.pokemonByName(pokemonName);
 
     if (!pokemon) {
       throw new Error("addToPokemonTrainer pokemon doesnt exist")
     }
-    if(this.trainerService.inFavouritePokemon(pokemonName)){
+    if (this.trainerService.inFavouritePokemon(pokemonName)) {
       trainer.pokemon = this.trainerService.removeFromFavourites(pokemonName);
     } else {
       this.trainerService.addToFavourites(pokemon)
@@ -47,22 +68,22 @@ export class TrainerPageService {
 
     this._loading = true;
 
-    const headers = new HttpHeaders ({
-      'content-type' : 'application/json',
-      'x-api-key' : apiKey
+    const headers = new HttpHeaders({
+      'content-type': 'application/json',
+      'x-api-key': apiKey
     })
 
     console.log("pokemon elementene i arrayet ", trainer.pokemon)
     return this.http.patch<Trainer>(`${apiTrainer}/${trainer.id}`, {
       pokemon: [...trainer.pokemon]
-      
+
     }, {
       headers
-    }) .pipe(tap((updateTrainer: Trainer) => {
+    }).pipe(tap((updateTrainer: Trainer) => {
       this.trainerService.trainer = updateTrainer;
 
     }),
-      finalize(()=> {
+      finalize(() => {
         this._loading = false;
 
       })
--- src/app/services/trainer-service.service.ts
+++ src/app/services/trainer-service.service.ts
@@ -7,11 +7,18 @@ import { Trainer } from '../models/trainer.models';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * A service that manages the current user's Pokemon trainer data.
+ */
 export class TrainerServiceService {
   private _trainer?: Trainer;
   constructor() {
     this._trainer = StorageUtil.storageRead<Trainer>(StorageKeys.Trainer);
   }
+
+  // Getter and setter
+
   get trainer(): Trainer | undefined {
     return this._trainer;
   } 
@@ -19,6 +26,12 @@ export class TrainerServiceService {
     StorageUtil.storageSave<Trainer>(StorageKeys.Trainer, pokemonTrainer!);
     this._trainer = pokemonTrainer;
   }
+
+   /**
+   * Determines whether the specified Pokemon is in the trainer page.
+   * @param {string} pokemonName - The name of the Pokemon to check.
+   * @returns True if the Pokemon is in the trainer favourites; otherwise, false.
+   */
 
   public inFavouritePokemon(pokemonName: any): boolean {
     if (this._trainer) {
@@ -31,11 +44,21 @@ export class TrainerServiceService {
     }
     return false;
   }
+  /**
+   * Adds the specified Pokemon to the trainer page.
+   * @param {Pokemon} newPokemon - The Pokemon to add.
+   */
   public addToFavourites(newPokemon: Pokemon): void {
     if (this._trainer) {
       this._trainer.pokemon.push(newPokemon)
     }
   }
+
+   /**
+   * Removes the specified Pokemon from the trainer page.
+   * @param {string} pokemonName - The name of the Pokemon to remove.
+   * @returns An array containing the remaining Pokemon in the trainer page.
+   */
 
   public removeFromFavourites(pokemonName: string): Pokemon[] {
     if (this._trainer) {
